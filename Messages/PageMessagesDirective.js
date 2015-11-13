36589d56df399d516cf93c715e7004a537d2b3ad
emmadiharish <hemmadi@apttus.com>
2015-11-12 16:00:58 -0800

    ajsbhd

--- Messages/PageMessagesDirective.js
+++ Messages/PageMessagesDirective.js
@@ -57,7 +57,7 @@
             // terminal: true,
             // scope: {}, // {} = isolate, true = child, false/undefined = no change
             controller: PageMessagesController,
-            controllerAs: 'messageCtrl',
+            controllerAs: 'pgMessageCtrl',
             // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
             // restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
             //template: '<div>pageHeader</div>',
