9d26131ebae9c040f9cacebcf4727942b8e1154a
emmadiharish <hemmadi@apttus.com>
2015-11-13 13:39:00 -0800

    jahsbf

---
+++ Controllers/paginationControlsCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+	angular.module('APTPS_ngCPQ').controller('paginationControlsCtrl', PaginationControlsCtrl);
+	
+	PaginationControlsCtrl.$inject = [
+		
+	];
+
+	function PaginationControlsCtrl() {
+		var paginationCtrl = this;
+	}
+
+})();
