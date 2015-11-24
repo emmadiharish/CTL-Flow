ec02b4a5f65a1b4ca0d8ebc5852d4b3cd9806b4a
emmadiharish <hemmadi@apttus.com>
2015-11-24 10:11:42 -0800

    ajshbd

--- Locations/SelectedServiceLocationDirective.js
+++ Locations/SelectedServiceLocationDirective.js
@@ -35,7 +35,7 @@
             // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
             restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
             //template: '<div>pageHeader</div>',
-            templateUrl: SystemConstants.baseUrl + "/Templates/"+{{title}}+".html",
+            templateUrl: SystemConstants.baseUrl + "/Templates/"+title+".html",
             // replace: true,
             // transclude: true,
             // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
