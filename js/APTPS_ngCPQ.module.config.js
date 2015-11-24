aed7e02f968f0ab45156c0c9536c7729040aa82e
emmadiharish <hemmadi@apttus.com>
2015-11-24 13:46:15 -0800

    asjbda

---
+++ js/APTPS_ngCPQ.module.config.js
@@ -0,0 +1,28 @@
+;(function() {
+    'use strict';
+
+    configBlock.$inject = [
+            '$provide',
+            'SystemConstants',
+            'paginationTemplateProvider'
+        ];
+
+    function configBlock($provide, SystemConstants, paginationTemplateProvider){
+        //A single pagination-controls template is used throught the app. This may be limiting.
+        paginationTemplateProvider.setPath(SystemConstants.baseUrl + '/Templates/pagination.html');
+
+        $provide.decorator("$exceptionHandler", ['$delegate', 'PageErrorDataService', function($delegate, PageErrorDataService){
+                return function(exception, cause){
+                    exception.message = 'Please contact Admin \n Message: '+exception.message;
+                    $delegate(exception, cause);
+                    
+                    // end progress bar so atleast user can abandon on hard-error.
+                    // BaseService.resetProgressBartinprogress();// end progress bar.
+                    
+                    PageErrorDataService.add(exception.message);
+            };
+        }])
+    }
+
+    angular.module('APTPS_ngCPQ').config(configBlock);
+})();
